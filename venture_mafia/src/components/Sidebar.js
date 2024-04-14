fcd00fcbe24e6ba90d1467298031d4e026eab7d0
johanhaha <johan.torssell@gmail.com>
2024-04-14 11:40:32 +0200

    Added IPO info to siddebar + changed structure on primary node info

--- venture_mafia/src/components/Sidebar.js
+++ venture_mafia/src/components/Sidebar.js
@@ -1,16 +1,30 @@
 import React, { useEffect } from "react";
-import { colours, opacity } from "../styling.js";
+import { colours } from "../styling.js";
 import targetOrg from "../data/targetOrg.json";
 import { ReactComponent as VMlogo } from "../VMlogo.svg";
 
-// Helper function to format as USD
+// Helper function to format as USD and abbreviate
 function formatToUSD(value) {
-  return new Intl.NumberFormat("en-US", {
-    style: "currency",
-    currency: "USD",
-    minimumFractionDigits: 0, // No decimal points
-    maximumFractionDigits: 0, // No decimal points
-  }).format(value);
+  let prefix = "$";
+  let suffix = "";
+  let abbreviatedNumber = value;
+  if (value >= 1e9) {
+    suffix = "bn";
+    abbreviatedNumber = value / 1e9;
+  } else if (value >= 1e6) {
+    suffix = "m";
+    abbreviatedNumber = value / 1e6;
+  } else if (value >= 1e3) {
+    suffix = "k";
+    abbreviatedNumber = value / 1e3;
+  }
+
+  const formatter = new Intl.NumberFormat("en-US", {
+    minimumFractionDigits: 0,
+    maximumFractionDigits: abbreviatedNumber < 10 ? 1 : 0,
+  });
+
+  return prefix + formatter.format(abbreviatedNumber) + suffix;
 }
 
 // Helper function to format epoch to date
@@ -38,25 +52,39 @@ function Sidebar({ selectedNodeData }) {
         padding: "30px 50px 30px 50px",
       }}
     >
-      <VMlogo style={{ stroke: colours.main.primary1, width: "60%", height: "auto", marginBottom: "100px" }} />
+      <VMlogo
+        style={{
+          stroke: colours.main.primary1,
+          width: "60%",
+          height: "auto",
+          marginBottom: "100px",
+        }}
+      />
       {!selectedNodeData && (
         <div>
-            <div style={{ display: "flex", alignItems: "center" }}>
-                <img
-                src={targetOrg.orgLogoUrl}
-                alt={targetOrg.orgName + " logo"}
-                style={{ width: "90px", height: "90px", objectFit: "cover", borderRadius: "45px" }}
-                />
-                <h1 style={{ marginLeft: "20px" }}>The {targetOrg.orgName} Mafia</h1>
-            </div>
-            <p>{targetOrg.shortDescription}</p>
-            <ul>
+          <div style={{ display: "flex", alignItems: "center" }}>
+            <img
+              src={targetOrg.orgLogoUrl}
+              alt={targetOrg.orgName + " logo"}
+              style={{
+                width: "90px",
+                height: "90px",
+                objectFit: "cover",
+                borderRadius: "45px",
+              }}
+            />
+            <h1 style={{ marginLeft: "20px" }}>
+              The {targetOrg.orgName} Mafia
+            </h1>
+          </div>
+          <p>{targetOrg.shortDescription}</p>
+          <ul>
             <li>
-                HQ: {targetOrg.orgCity}, {targetOrg.orgCountryCode}
+              HQ: {targetOrg.orgCity}, {targetOrg.orgCountryCode}
             </li>
             <li>Total funding: {formatToUSD(targetOrg.totalFundingUsd)}</li>
             <li>Founded: {epochToDate(targetOrg.foundedOn)}</li>
-            </ul>
+          </ul>
         </div>
       )}
       {/* Displaying information about selected primary nodes */}
@@ -66,12 +94,18 @@ function Sidebar({ selectedNodeData }) {
             <img
               src={selectedNodeData.personLogoUrl}
               alt={selectedNodeData.name + " logo"}
-              style={{ width: "90px", height: "90px", objectFit: "cover", borderRadius: "45px" }}
+              style={{
+                width: "90px",
+                height: "90px",
+                objectFit: "cover",
+                borderRadius: "45px",
+              }}
             />
             <h1 style={{ marginLeft: "20px" }}>{selectedNodeData.name}</h1>
           </div>
           <h2>Role at {targetOrg.orgName}</h2>
-          {selectedNodeData.jobTitle}, {epochToDate(selectedNodeData.startedOn)} to {epochToDate(selectedNodeData.endedOn)}
+          {selectedNodeData.jobTitle}, {epochToDate(selectedNodeData.startedOn)}{" "}
+          to {epochToDate(selectedNodeData.endedOn)}
         </div>
       )}
       {/* Displaying information about selected secondary nodes */}
@@ -88,12 +122,35 @@ function Sidebar({ selectedNodeData }) {
 
           <p>{selectedNodeData.shortDescription}</p>
           <ul>
-          <li>
-            HQ: {selectedNodeData.orgCity}, {selectedNodeData.orgCountryCode}
-          </li>
-          <li>Total funding: {formatToUSD(selectedNodeData.totalFundingUsd)}</li>
-          <li>Founded: {epochToDate(selectedNodeData.foundedOn)}</li>
+            <li>
+              HQ: {selectedNodeData.orgCity}, {selectedNodeData.orgCountryCode}
+            </li>
+            <li>
+              Total funding: {formatToUSD(selectedNodeData.totalFundingUsd)}
+            </li>
+            <li>Founded: {epochToDate(selectedNodeData.foundedOn)}</li>
           </ul>
+          {selectedNodeData &&
+            selectedNodeData.type === "secondary" &&
+            selectedNodeData.exitType === "ipo" && (
+              <div>
+                {selectedNodeData.name} went public{" "}
+                {epochToDate(selectedNodeData.exitDate)}
+                {selectedNodeData.exitValuation &&
+                  " at a value of " +
+                    formatToUSD(selectedNodeData.exitValuation)}
+              </div>
+            )}
+          {selectedNodeData &&
+            selectedNodeData.type === "secondary" &&
+            selectedNodeData.exitType === "acquisition" && (
+              <div>
+                {selectedNodeData.name} was acquired on{" "}
+                {epochToDate(selectedNodeData.exitDate)} by{" "}
+                {selectedNodeData.acquirerName} for{" "}
+                {formatToUSD(selectedNodeData.exitValuation)}
+              </div>
+            )}
         </div>
       )}
     </div>
