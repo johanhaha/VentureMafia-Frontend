adb9c5a60f0fd8d55260daaf9b6431fca6300ac0
johanhaha <johan.torssell@gmail.com>
2024-08-29 10:23:19 +0200

    Break out PrimaryNodeSidebarInfo component from PrimaryNodeSidebarContent

--- src/components/Sidebar/PrimaryNodeSidebarContent.js
+++ src/components/Sidebar/PrimaryNodeSidebarContent.js
@@ -1,7 +1,12 @@
 import ProfileImage from "./ProfileImage.js";
+import PrimaryNodeSidebarInfo from "./PrimaryNodeSidebarInfo.js";
 import { text } from "../../styling.js";
 
-function PrimaryNodeSidebarContent({ targetOrg, selectedNodeData, epochToDate }) {
+function PrimaryNodeSidebarContent({
+  targetOrg,
+  selectedNodeData,
+  epochToDate,
+}) {
   return (
     <div>
       <div
@@ -18,25 +23,11 @@ function PrimaryNodeSidebarContent({ targetOrg, selectedNodeData, epochToDate })
         />
         <h1 style={{ ...text.header }}>{selectedNodeData.name}</h1>
       </div>
-      <h2 style={text.subHeader}>Role at {targetOrg.orgName}</h2>
-      <p style={text.contentFocus}>
-        {selectedNodeData.jobTitle}
-        {/* If both start and end dates exist */}
-        {selectedNodeData.startedOn &&
-          selectedNodeData.endedOn &&
-          ", " +
-            epochToDate(selectedNodeData.startedOn) +
-            " to " +
-            epochToDate(selectedNodeData.endedOn)}
-        {/* If only start date exist */}
-        {selectedNodeData.startedOn &&
-          !selectedNodeData.endedOn &&
-          ", since " + epochToDate(selectedNodeData.startedOn)}
-        {/* If only end date exist */}
-        {!selectedNodeData.startedOn &&
-          selectedNodeData.endedOn &&
-          ", until " + epochToDate(selectedNodeData.endedOn)}
-      </p>
+      <PrimaryNodeSidebarInfo
+        orgName={targetOrg.orgName}
+        selectedNodeData={selectedNodeData}
+        epochToDate={epochToDate}
+      />
     </div>
   );
 }
---
+++ src/components/Sidebar/PrimaryNodeSidebarInfo.js
@@ -0,0 +1,28 @@
+import { text } from "../../styling.js";
+
+function PrimaryNodeSidebarInfo({ orgName, selectedNodeData, epochToDate }) {
+  return (
+    <div>
+      <h2 style={text.subHeader}>Role at {orgName}</h2>
+      <p style={text.contentFocus}>
+        {selectedNodeData.jobTitle}
+        {/* If both start and end dates exist */}
+        {selectedNodeData.startedOn &&
+          selectedNodeData.endedOn &&
+          ", " +
+            epochToDate(selectedNodeData.startedOn) +
+            " to " +
+            epochToDate(selectedNodeData.endedOn)}
+        {/* If only start date exist */}
+        {selectedNodeData.startedOn &&
+          !selectedNodeData.endedOn &&
+          ", since " + epochToDate(selectedNodeData.startedOn)}
+        {/* If only end date exist */}
+        {!selectedNodeData.startedOn &&
+          selectedNodeData.endedOn &&
+          ", until " + epochToDate(selectedNodeData.endedOn)}
+      </p>
+    </div>
+  );
+}
+export default PrimaryNodeSidebarInfo;
