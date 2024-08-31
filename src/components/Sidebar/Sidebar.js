412e10944d2807efe78485169fb98a5d9e92f7e7
johanhaha <johan.torssell@gmail.com>
2024-08-31 11:49:46 +0200

    Break out sidebar utils

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -2,40 +2,9 @@ import React, { useState, useEffect } from "react";
 import TargetOrgContent from "./TargetOrgContent.js";
 import PrimaryNodeContent from "./PrimaryNodeContent.js";
 import SecondaryNodeContent from "./SecondaryNodeContent.js";
+import { epochToDate, formatToUSD} from "./utils.js";
 import { colours } from "../../styling.js";
 import { ReactComponent as VMlogo } from "../../assets/VMlogo.svg";
-
-// Helper function to format as USD and abbreviate
-function formatToUSD(value) {
-  let prefix = "$";
-  let suffix = "";
-  let abbreviatedNumber = value;
-  if (value >= 1e9) {
-    suffix = "bn";
-    abbreviatedNumber = value / 1e9;
-  } else if (value >= 1e6) {
-    suffix = "m";
-    abbreviatedNumber = value / 1e6;
-  } else if (value >= 1e3) {
-    suffix = "k";
-    abbreviatedNumber = value / 1e3;
-  }
-
-  const formatter = new Intl.NumberFormat("en-US", {
-    minimumFractionDigits: 0,
-    maximumFractionDigits: abbreviatedNumber < 10 ? 1 : 0,
-  });
-
-  return prefix + formatter.format(abbreviatedNumber) + suffix;
-}
-
-// Helper function to format epoch to date
-function epochToDate(epoch) {
-  return new Date(epoch).toLocaleDateString("en-US", {
-    year: "numeric",
-    // month: "long",
-  });
-}
 
 function Sidebar({
   availableOrgs,
