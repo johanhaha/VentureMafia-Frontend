5fe110c1e8fba2f6744c6a5678b169926d3e76d4
johanhaha <johan.torssell@gmail.com>
2024-09-01 12:12:12 +0200

    Moved footer and main content layout to Layout component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,8 @@
-import React, { useState, useRef, useEffect } from "react";
-import "./App.css";
-import { colours, text } from "./styling.js";
-import { analytics, logEvent } from "./firebase.js";
+import { useState, useRef, useEffect } from "react";
+import Layout from "./components/Layout.js";
 import NetworkGraph from "./components/NetworkGraph/NetworkGraph.js";
 import Sidebar from "./components/Sidebar/Sidebar.js";
-import Footer from "./components/Footer.js";
+import "./App.css";
 
 var targetOrgUuid = "f7a3ff7d-5a7c-71c7-383d-6883b355f8b0"; // Set the default target uuid
 const BASE_URL = process.env.REACT_APP_BASE_URL || "http://localhost:3000";
@@ -24,24 +22,27 @@ function App() {
   // Helper fucntion to import data
   async function loadData(targetOrgUuid) {
     try {
-      var [availableOrgsRaw, targetOrgRaw, alumniInfoRaw, subsequentOrgsInfoRaw, relationsRaw] =
-        await Promise.all([
-          fetch(`${BASE_URL}/available_orgs`).then((res) =>
-            res.json()
-          ),
-          fetch(`${BASE_URL}/target_org/${targetOrgUuid}`).then((res) =>
-            res.json()
-          ),
-          fetch(`${BASE_URL}/alumni_info/${targetOrgUuid}`).then((res) =>
-            res.json()
-          ),
-          fetch(`${BASE_URL}/subsequent_orgs_info/${targetOrgUuid}`).then(
-            (res) => res.json()
-          ),
-          fetch(`${BASE_URL}/relations/${targetOrgUuid}`).then((res) =>
-            res.json()
-          ),
-        ]);
+      var [
+        availableOrgsRaw,
+        targetOrgRaw,
+        alumniInfoRaw,
+        subsequentOrgsInfoRaw,
+        relationsRaw,
+      ] = await Promise.all([
+        fetch(`${BASE_URL}/available_orgs`).then((res) => res.json()),
+        fetch(`${BASE_URL}/target_org/${targetOrgUuid}`).then((res) =>
+          res.json()
+        ),
+        fetch(`${BASE_URL}/alumni_info/${targetOrgUuid}`).then((res) =>
+          res.json()
+        ),
+        fetch(`${BASE_URL}/subsequent_orgs_info/${targetOrgUuid}`).then((res) =>
+          res.json()
+        ),
+        fetch(`${BASE_URL}/relations/${targetOrgUuid}`).then((res) =>
+          res.json()
+        ),
+      ]);
       setAvailableOrgs(availableOrgsRaw.data);
       setTargetOrg(targetOrgRaw.data);
       setAlumniInfo(alumniInfoRaw.data);
@@ -86,74 +87,32 @@ function App() {
   };
 
   return (
-    <div style={{ backgroundColor: colours.neutrals.background }}>
-      <div
-        style={{
-          display: "grid",
-          gridTemplateColumns: "1fr 3fr",
-          height: "calc(100vh - 50px)", // Adjust height to account for footer
-          width: "100vw",
-        }}
-      >
-        {/* Left panel */}
-        <div>
-          <Sidebar
-            availableOrgs={availableOrgs}
+    <Layout>
+      {/* Left panel */}
+      <div>
+        <Sidebar
+          availableOrgs={availableOrgs}
+          targetOrg={targetOrg}
+          onTargetOrgSelect={handleTargetOrgSelect}
+          selectedNodeData={selectedNode}
+        />
+      </div>
+      {/* NetworkGraph */}
+      <div ref={containerRef}>
+        {networkGraphDimensions ? ( // Checks if networkGraphDimensions has been set to ensure proper size of graph
+          <NetworkGraph
             targetOrg={targetOrg}
-            onTargetOrgSelect={handleTargetOrgSelect}
-            selectedNodeData={selectedNode}
+            alumniInfo={alumniInfo}
+            subsequentOrgsInfo={subsequentOrgsInfo}
+            relations={relations}
+            onNodeSelect={handleNodeSelect}
+            networkGraphDimensions={networkGraphDimensions}
           />
-        </div>
-        {/* NetworkGraph */}
-        <div ref={containerRef}>
-          {networkGraphDimensions ? ( // Checks if networkGraphDimensions has been set to ensure proper size of graph
-            <NetworkGraph
-              targetOrg={targetOrg}
-              alumniInfo={alumniInfo}
-              subsequentOrgsInfo={subsequentOrgsInfo}
-              relations={relations}
-              onNodeSelect={handleNodeSelect}
-              networkGraphDimensions={networkGraphDimensions}
-            />
-          ) : (
-            <div> Loading graph... </div>
-          )}
-        </div>
+        ) : (
+          <div> Loading graph... </div>
+        )}
       </div>
-      {/* Footer */}
-      <div
-        style={{
-          display: "grid",
-          gridTemplateColumns: "1fr 3fr",
-          width: "100vw",
-          height: "50px",
-          overflow: "hidden",
-        }}
-      >
-        <p
-          style={{
-            ...text.contentFocus,
-            paddingLeft: "20px",
-            fontSize: "20px",
-          }}
-        >
-          <a
-            href="https://www.linkedin.com/in/johan-torssell/"
-            target="_blank"
-            rel="noreferrer"
-            style={{ textDecoration: "none", color: "inherit" }}
-            onClick={(event) => {
-              logEvent(analytics, "johan_linkedin_click"); // Log click to LinkedIn profile
-            }}
-          >
-            &copy;Johan Torssell
-          </a>
-        </p>
-        <div style={{ height: "100%" }}>
-          <Footer />
-        </div>
-      </div>
-    </div>
+    </Layout>
   );
 }
 
