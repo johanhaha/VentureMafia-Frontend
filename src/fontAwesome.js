35fe988dc418ffde0eb97ba1b8aa2ecd01455cfa
johanhaha <johan.torssell@gmail.com>
2024-10-10 20:30:45 +0200

    Initial commit for create-next-app

--- src/fontAwesome.js
+++
@@ -1,8 +0,0 @@
-import { library } from "@fortawesome/fontawesome-svg-core";
-import { faUser as fasUser, faBuilding as fasBuilding } from "@fortawesome/free-solid-svg-icons";
-import { faUser as farUser, faBuilding as farBuilding } from "@fortawesome/free-regular-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-
-library.add(fasUser, farUser, fasBuilding, farBuilding);
-
-export { FontAwesomeIcon };
