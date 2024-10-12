d7169efe08323dfc15db171e4a10bf19a58e5a40
johanhaha <johan.torssell@gmail.com>
2024-10-12 13:45:40 +0200

    Surpressed warning for react-select id discrepancy

--- app/components/Sidebar/TitleTargetOrg.js
+++ app/components/Sidebar/TitleTargetOrg.js
@@ -1,6 +1,11 @@
-import DropdownTargetOrg from "./DropdownTargetOrg";
+import dynamic from 'next/dynamic';
 import Tooltip from "./Tooltip.js";
 import { text } from "../../styling.js";
+
+// Dynamically import DropdownTargetOrg without SSR
+const DropdownTargetOrg = dynamic(() => import("./DropdownTargetOrg"), {
+  ssr: false, // Disable server-side rendering for this component
+});
 
 function TitleTargetOrg({ availableOrgs, targetOrg }) {
   const tooltipDefaultOpen = true;
