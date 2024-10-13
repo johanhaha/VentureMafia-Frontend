04e972a9fa4ef3c810f2c0460dcb4548b8048119
johanhaha <johan.torssell@gmail.com>
2024-10-13 21:46:17 +0200

    Enabled import of SVGs as components

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    webpack(config) {
+      config.module.rules.push({
+        test: /\.svg$/, // Target SVG files
+        use: ["@svgr/webpack"], // Use the SVGR loader to import as React components
+      });
+  
+      return config;
+    },
+  };
+  
+  export default nextConfig;
+  
